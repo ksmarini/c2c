ab09c7eb9b1e5312256d631a9d73fb365ac6c3ec
Krisofferson Marini <60137002+ksmarini@users.noreply.github.com>
2023-11-26 17:21:35 +0000

    iniciando a config do servidor

--- server/c2c.go
+++ server/c2c.go
@@ -1,5 +1,26 @@
-package server
+package main
+
+import ("log"
+		"net")
 
 func main() {
-	println("Entrei em execução.")
+	log.Println("Entrei em Execução.")
+	startListener("9090")
+}
+
+func startListener(port string) {
+	listener, err := net.Listen("tcp", "0.0.0.0:" + port)
+
+	if err != nil {
+		log.Fatal("Erro ao iniciar o listener: ", err.Error())
+	} else {
+		canal, err := listener.Accept()
+		defer canal.Close()
+
+		if err != nil {
+			log.Println("Erro em um novo canal: ", err.Error())
+		} 
+
+		log.Println("Nova conexão: ", canal.RemoteAddr().String())
+	}
 }
